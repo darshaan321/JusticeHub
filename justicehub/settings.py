c5ba13598c58c03b7e6bc95bcaeff835a1bd623f
darshan <darshankote81@gmail.com>
2026-05-25 15:11:19 +0530

    fixed allowed hosts

--- justicehub/settings.py
+++ justicehub/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l2v=-s#q&p=st&5md1xsjx67n0gcpvm2u1xau%86ljwuup3xlw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
 
 
+
+ALLOWED_HOSTS = ['justicehub-uop5.onrender.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
